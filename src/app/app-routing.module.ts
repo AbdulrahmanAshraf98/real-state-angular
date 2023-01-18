221b8388a5149b2d099a6b8664afbbf69260ea1a
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2023-01-18 18:43:20 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { BuildingsComponent } from './pages/buildings/buildings.component';
+import { UnitsComponent } from './pages/units/units.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'buildings', component: BuildingsComponent },
+  { path: 'units', component: UnitsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { NavbarItemComponent } from './shared/navbar/navbar-item/navbar-item.component';
 import { NavbarItemsComponent } from './shared/navbar/navbar-items/navbar-items.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { BuildingsComponent } from './pages/buildings/buildings.component';
+import { UnitsComponent } from './pages/units/units.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +17,10 @@ import { NavbarItemsComponent } from './shared/navbar/navbar-items/navbar-items.
     NavbarComponent,
     NavbarItemComponent,
     NavbarItemsComponent,
+    HomeComponent,
+    ProjectsComponent,
+    BuildingsComponent,
+    UnitsComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
