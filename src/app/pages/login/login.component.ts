720d5ab8c3ca1dc27854cfe8fccc9806aaf9d5cb
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2023-01-19 17:08:23 +0200

    project component

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { LoginInterface } from 'src/app/interface/login';
+import { GlobalService } from 'src/app/services/global.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnDestroy {
+  subscription: any;
+  constructor(private global: GlobalService) {}
+  model: LoginInterface = {
+    email: '',
+    password: '',
+  };
+  error = {};
+  submitHandler(f: NgForm) {
+    if (f.invalid) return;
+    this.subscription = this.global.post('user/login', this.model).subscribe(
+      (response) => {
+        localStorage.setItem('token', response.data.token);
+      },
+      (error) => console.log(error)
+    );
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,19 +1,26 @@
 import { projectInterface } from 'src/app/interface/projectInterface';
 import { GlobalService } from './../../services/global.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css'],
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent implements OnInit, OnDestroy {
+  subscription: any;
   projects: projectInterface[] = [];
   constructor(private global: GlobalService) {}
+
   ngOnInit(): void {
-    this.global.get('project/').subscribe((responseData) => {
-      this.projects = responseData.data;
-      console.log(this.projects);
-    });
+    this.subscription = this.global
+      .get('project/')
+      .subscribe((responseData) => {
+        this.projects = responseData.data;
+        console.log(this.projects);
+      });
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
